5bb1e292a75184f232b1c9be78ca93f270677a3f
Ushlerdop <axalimon@gmail.com>
2021-11-19 10:27:35 +1000

    add functions for addEventListeners

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ function getRandomColor() {
     return `hsl(${Math.random() * 360}, 95%, 60%)`
 }
 
+function setColor(e) {
+    let color = getRandomColor();
+    e.target.style.backgroundColor = `${color}`;
+    e.target.style.boxShadow = `0 0 2px ${color}, 0 0 14px ${color}`;
+}
+
+function removeColor(e) {
+    e.target.style.backgroundColor = ``;
+    e.target.style.boxShadow = ``;
+}
+
 function createCell() {    
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.addEventListener('mouseenter', (e) => {
-        let color = getRandomColor();
-        e.target.style.backgroundColor = `${color}`;
-        e.target.style.boxShadow = `0 0 2px ${color}, 0 0 14px ${color}`;
-    })
-    cell.addEventListener('mouseleave', (e) => {
-        e.target.style.backgroundColor = ``;
-        e.target.style.boxShadow = ``;
-    })
+    cell.addEventListener('mouseenter', setColor);
+    cell.addEventListener('mouseleave', removeColor);
     return cell;
 }
 
